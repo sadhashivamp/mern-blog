95b4e72f94562cbb94878ce194912bb14536616a
sadhashivamp <ramulu320p@gmail.com>
2024-03-20 19:43:07 +0530

    created and run server

---
+++ api/index.js
@@ -0,0 +1,7 @@
+import express from 'express';
+
+const app = express();
+
+app.listen(3000, () => {
+    console.log("Im sadha shivam creating api for client....")
+})
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,12 +6,14 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
 import DashboardPage from "./pages/Dashboard";
+import Header from './components/Header';
 
 function App() {
 
   return (
     <>
     <Router>
+      <Header />
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/about" element={<About />} />
