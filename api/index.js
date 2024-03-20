2f4ea2aad792c9fef165e6713a4346045dd9d494
sadhashivamp <ramulu320p@gmail.com>
2024-03-20 20:08:12 +0530

    connected to the mongodb

--- api/index.js
+++ api/index.js
@@ -1,7 +1,16 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("MongoDb connected");
+  })
+  .catch((error) => console.log(error));
 app.listen(3000, () => {
-    console.log("Im sadha shivam creating api for client....")
-})
+  console.log("Im sadha shivam creating api for client....");
+});
